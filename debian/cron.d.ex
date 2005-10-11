cfaf7289ea95b52df2e7c88d4cba26a3ccd5d745
John Goerzen <jgoerzen@complete.org>
2005-10-11 00:26:14 +0000

    Removed unnecessary .ex files

--- debian/cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the libarchive package
-#
-0 4	* * *	root	libarchive_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(libarchive):needs="X11|text|vc|wm" section="Apps/see-menu-manual"\
-  title="libarchive" command="/usr/bin/libarchive"
